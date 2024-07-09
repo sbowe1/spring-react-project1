d5bd50726e34244f0c725e7b746ce0d3a6f699cd
Sidney Bowe <sidneyb1202@gmail.com>
2024-07-09 16:56:05 -0700

    backend setup

---
+++ p1-backend/src/main/java/com/example/p1_backend/P1BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.p1_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P1BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P1BackendApplication.class, args);
+	}
+
+}
