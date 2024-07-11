180198f00a30d496be2c592768aa7a0038ac0a17
Sidney Bowe <sidneyb1202@gmail.com>
2024-07-10 19:18:16 -0700

    enabled jpa repository, entity scan, and component scan

--- p1-backend/src/main/java/com/example/p1_backend/P1BackendApplication.java
+++ p1-backend/src/main/java/com/example/p1_backend/P1BackendApplication.java
@@ -2,8 +2,14 @@ package com.example.p1_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan("com.example.p1_backend")
+@EntityScan("com.example.p1_backend.models")
+@EnableJpaRepositories("com.example.p1_backend.repositories")
 public class P1BackendApplication {
 
 	public static void main(String[] args) {
