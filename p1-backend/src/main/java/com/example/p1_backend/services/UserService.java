61e4ea379cc9acc4867095e73c25caeb324f88e8
pekellogg <peyton846@revature.net>
2024-07-10 17:43:03 -0700

    adds @Service class UserService

---
+++ p1-backend/src/main/java/com/example/p1_backend/services/UserService.java
@@ -0,0 +1,25 @@
+package com.example.p1_backend.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.p1_backend.models.User;
+import com.example.p1_backend.repositories.UserDao;
+
+@Service
+public class UserService {
+    private final UserDao uDao;
+
+    public UserService(UserDao uDao) {
+        this.uDao = uDao;
+    }
+
+    public List<User> getAll() {
+        return uDao.findAll();
+    }
+
+    public User findByUserId(int userId) {
+        return uDao.findByUserId(userId).get();
+    }
+}
