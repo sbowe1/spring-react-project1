a7d70622b87c5c641e136fe0bcbb251cce8cca89
pekellogg <peyton846@revature.net>
2024-07-10 17:44:01 -0700

    adds @Repository interface UserDao extends JpaRepository

---
+++ p1-backend/src/main/java/com/example/p1_backend/repositories/UserDao.java
@@ -0,0 +1,13 @@
+package com.example.p1_backend.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.p1_backend.models.User;
+
+@Repository
+public interface UserDao extends JpaRepository<User, Integer> {
+    Optional<User> findByUserId(int userId);
+}
