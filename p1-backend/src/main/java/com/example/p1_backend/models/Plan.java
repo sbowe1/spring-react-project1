b1a0dcfeada161233c4acbc387caf99a1ab69e88
Sidney Bowe <sidneyb1202@gmail.com>
2024-07-10 19:19:11 -0700

    all entites, need to talk about relationship btwn user and plan

---
+++ p1-backend/src/main/java/com/example/p1_backend/models/Plan.java
@@ -0,0 +1,27 @@
+package com.example.p1_backend.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.stereotype.Component;
+
+@Entity
+@Component
+@Data
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Plan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "planId")
+    private int planId;
+
+    private String name;
+
+    public Plan(int planId, String name) {
+        this.planId = planId;
+        this.name = name;
+    }
+}
---
+++ p1-backend/src/main/java/com/example/p1_backend/models/Topic.java
@@ -0,0 +1,34 @@
+package com.example.p1_backend.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.stereotype.Component;
+
+@Entity
+@Component
+@Data
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Topic {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "topicId")
+    private int topicId;
+
+    private String title;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "planId")
+    private Plan plan;
+
+    private boolean status;
+
+    public Topic(int topicId, String title, Plan plan, boolean status) {
+        this.topicId = topicId;
+        this.title = title;
+        this.plan = plan;
+        this.status = status;
+    }
+}
