feb4606a9b142da0808503a066e22e2a38d8b2d6
pekellogg <peyton846@revature.net>
2024-08-18 14:11:41 -0700

    adds password validations which dynamically update password requirements checkboxes displayed to user

--- p1-frontend/app/users/register/register-form.tsx
+++ p1-frontend/app/users/register/register-form.tsx
@@ -4,17 +4,60 @@ import { useState } from "react";
 
 const label = { inputProps: { 'aria-label': 'Checkbox' } };
 
+// validations
+
+// - email:
+    // not ""
+    // is not only whitespaces
+    // valid email regex
+    // "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$"
+    // unique
+
+// - password:
+    // 8-16 characters
+    // at least one special character ! # $ ^ & * - _
+    // at least one capital letter
+
+// - username: unique
+
+const specialChars = ["!", "#", "$", "^", "&", "*", "-", "_"];
+
 export function RegisterForm() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [name, setName] = useState("");
     const [message, setMessage] = useState("");
-    const [checked, setChecked] = useState(false);
+    const [passwordChecks, setPasswordChecks] = useState({
+        length: false,
+        specialChar: false,
+        capitalLetter: false
+    });
     const [createDisabled, setCreateDisabled] = useState(true);
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      setChecked(event.target.checked);
-    };
+    //   setChecked(event.target.checked); // Checkbox
+        if (event.target.id === "name") {
+            // query backend for username uniqueness
+            setName(event.target.value)
+        };
+
+        if (event.target.id === "email") {
+            setEmail(event.target.value)
+        };
+
+        if (event.target.id === "password") {
+            setPassword(event.target.value)
+            // invoke setPassword again to have the password set above available for evaluation
+            setPassword((prevPassword) => {
+                setPasswordChecks({
+                    capitalLetter: event.target.value !== event.target.value.toLowerCase(),
+                    specialChar: specialChars.some((char) => event.target.value.includes(char)),
+                    length: event.target.value.length >= 8 && event.target.value.length <= 16
+                })
+                return prevPassword
+            })
+        };
+    }
 
     function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
@@ -51,20 +94,67 @@ export function RegisterForm() {
 
     return (
         <form onSubmit={handleSubmit}>
-            <FormGroup >
-                <TextField style={{backgroundColor: "#E6E0E9"}} id="name" name="name" placeholder="Miranda Hobbes" label="Name" variant="filled" value={name} onChange={({ target }) => setName(target.value)}/><br/><br/>
+            <FormGroup>
+                <TextField
+                    style={{backgroundColor: "#E6E0E9"}}
+                    id="name"
+                    name="name"
+                    placeholder="Miranda Hobbes"
+                    label="Name"
+                    variant="filled"
+                    value={name}
+                    required
+                    onChange={handleChange}
+                /><br/><br/>
 
-                <TextField style={{backgroundColor: "#E6E0E9"}} id="email" name="email" placeholder="mirmir@gmail.com" label="Email" variant="filled" value={email} onChange={({ target }) => setEmail(target.value)}/><br/><br/>
+                <TextField
+                    style={{backgroundColor: "#E6E0E9"}}
+                    id="email"
+                    name="email"
+                    placeholder="mirmir@gmail.com"
+                    label="Email"
+                    variant="filled"
+                    value={email}
+                    required
+                    onChange={handleChange}
+                /><br/><br/>
 
-                <TextField style={{backgroundColor: "#E6E0E9"}} id="password" name="password" type="password" label="Password" placeholder="Password_" variant="filled" value={password} onChange={({ target }) => setPassword(target.value)}/><br/><br/>
+                <TextField
+                    style={{backgroundColor: "#E6E0E9"}}
+                    id="password"
+                    name="password"
+                    type="password"
+                    label="Password"
+                    placeholder="Password_"
+                    variant="filled"
+                    value={password}
+                    required
+                    onChange={handleChange}
+                /><br/><br/>
+
                 Requirements<br/>
 
-                <FormControlLabel control={<Checkbox checked={checked} onChange={handleChange} disabled={true}/>} label="8-16 characters" />
-                <FormControlLabel control={<Checkbox checked={checked} onChange={handleChange} disabled={true}/>} label={"At least one special character" + "\n" + "! # $ ^ & * - _"} />
-                <FormControlLabel control={<Checkbox checked={checked} onChange={handleChange} disabled={true}/>} label="At least one capital letter" />
+                <FormControlLabel
+                    control={
+                        <Checkbox checked={passwordChecks.length} disabled={true}
+                    />}
+                    label="8-16 characters"
+                />
+                <FormControlLabel
+                    control={
+                        <Checkbox checked={passwordChecks.specialChar} disabled={true}
+                    />}
+                    label={"At least one special character" + "\n" + "! # $ ^ & * - _"}
+                />
+                <FormControlLabel
+                    control={
+                        <Checkbox checked={passwordChecks.capitalLetter} disabled={true}
+                    />}
+                    label="At least one capital letter"
+                />
 
-                <Button type="submit" variant="contained" disabled={createDisabled}>Create</Button><br/>
-                <Button type="submit" variant="contained">Cancel</Button><br/><br/>
+                <Button type="submit" variant="contained" disabled={createDisabled}>Create</Button><br/> 
+                <Button variant="contained">Cancel</Button><br/><br/>
                 {message != "" ? message : null}
             </FormGroup>
         </form>
