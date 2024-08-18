3ba7e27065bf4adc18e93454eaedaefbaaa2686a
pekellogg <peyton846@revature.net>
2024-08-17 17:34:06 -0700

    adds inline styling to input tag's background color

--- p1-frontend/app/users/register/register-form.tsx
+++ p1-frontend/app/users/register/register-form.tsx
@@ -10,6 +10,7 @@ export function RegisterForm() {
     const [name, setName] = useState("");
     const [message, setMessage] = useState("");
     const [checked, setChecked] = useState(false);
+    const [createDisabled, setCreateDisabled] = useState(true);
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
       setChecked(event.target.checked);
@@ -51,18 +52,18 @@ export function RegisterForm() {
     return (
         <form onSubmit={handleSubmit}>
             <FormGroup >
-                <TextField id="name" name="name" placeholder="Miranda Hobbes" label="Name" variant="filled" value={name} onChange={({ target }) => setName(target.value)}/><br/><br/>
+                <TextField style={{backgroundColor: "#E6E0E9"}} id="name" name="name" placeholder="Miranda Hobbes" label="Name" variant="filled" value={name} onChange={({ target }) => setName(target.value)}/><br/><br/>
 
-                <TextField id="email" name="email" placeholder="mirmir@gmail.com" label="Email" variant="filled"  value={email} onChange={({ target }) => setEmail(target.value)}/><br/><br/>
+                <TextField style={{backgroundColor: "#E6E0E9"}} id="email" name="email" placeholder="mirmir@gmail.com" label="Email" variant="filled" value={email} onChange={({ target }) => setEmail(target.value)}/><br/><br/>
 
-                <TextField id="password" name="password" type="password" placeholder="Password_" label="Password" variant="filled"  value={password} onChange={({ target }) => setPassword(target.value)}/><br/><br/>
+                <TextField style={{backgroundColor: "#E6E0E9"}} id="password" name="password" type="password" label="Password" placeholder="Password_" variant="filled" value={password} onChange={({ target }) => setPassword(target.value)}/><br/><br/>
                 Requirements<br/>
 
                 <FormControlLabel control={<Checkbox checked={checked} onChange={handleChange} disabled={true}/>} label="8-16 characters" />
                 <FormControlLabel control={<Checkbox checked={checked} onChange={handleChange} disabled={true}/>} label={"At least one special character" + "\n" + "! # $ ^ & * - _"} />
                 <FormControlLabel control={<Checkbox checked={checked} onChange={handleChange} disabled={true}/>} label="At least one capital letter" />
 
-                <Button type="submit" variant="contained" >Create</Button><br/> {/* // disabled={true} */}
+                <Button type="submit" variant="contained" disabled={createDisabled}>Create</Button><br/>
                 <Button type="submit" variant="contained">Cancel</Button><br/><br/>
                 {message != "" ? message : null}
             </FormGroup>
