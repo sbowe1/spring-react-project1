224976994c2e37be70dc034f8f401282ee45cf0b
pekellogg <peyton846@revature.net>
2024-09-06 12:02:19 -0700

    adds javadocs to all Resource entity classes

--- p1-backend/src/main/java/com/example/p1_backend/controllers/ResourceController.java
+++ p1-backend/src/main/java/com/example/p1_backend/controllers/ResourceController.java
@@ -1,14 +1,22 @@
 package com.example.p1_backend.controllers;
 
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.p1_backend.models.dtos.InResourceDto;
 import com.example.p1_backend.models.dtos.OutResourceDto;
 import com.example.p1_backend.services.ResourceService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import static org.springframework.http.HttpStatus.CREATED;
-import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping("resources")
@@ -22,7 +30,12 @@ public class ResourceController {
 		this.rs = rs;
 	}
 
-	// CREATE
+	/**
+	 * Creates a new resource without a subtopic.
+	 * @param	topicId
+	 * @param	resourceDto
+	 * @return	OutResourceDto
+	 */
 	@PostMapping("{topicId}")
 	public ResponseEntity<OutResourceDto> createNoSubtopic(@PathVariable int topicId,
 			@RequestBody InResourceDto resourceDto) {
@@ -30,6 +43,13 @@ public class ResourceController {
 		return new ResponseEntity<>(resource, CREATED);
 	}
 
+	/**
+	 * Creates a new resource with a subtopic.
+	 * @param	topicId
+	 * @param	subtopicId
+	 * @param	resourceDto
+	 * @return	OutResourceDto
+	 */
 	@PostMapping("{topicId}/{subtopicId}")
 	public ResponseEntity<OutResourceDto> createWithSubtopic(@PathVariable int topicId, @PathVariable int subtopicId,
 			@RequestBody InResourceDto resourceDto) {
@@ -37,14 +57,23 @@ public class ResourceController {
 		return new ResponseEntity<>(resource, CREATED);
 	}
 
-	// READ
+	/**
+	 * Reads a resource by id.
+	 * @param	resourceId
+	 * @return	OutResourceDto
+	 */
 	@GetMapping("{resourceId}")
 	public ResponseEntity<OutResourceDto> readResource(@PathVariable int resourceId) {
 		OutResourceDto resource = rs.readResource(resourceId);
 		return new ResponseEntity<>(resource, OK);
 	}
 
-	// UPDATE
+	/**
+	 * Updates a resource.
+	 * @param	resourceId
+	 * @param	inResourceDto
+	 * @return	OutResourceDto
+	 */
 	@PatchMapping("{resourceId}")
 	public ResponseEntity<OutResourceDto> updateResoure(@PathVariable int resourceId,
 			@RequestBody InResourceDto inResourceDto) {
